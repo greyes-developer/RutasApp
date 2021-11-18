f4cd455e1ad139c827e82d618ede9f33a2d2f05b
Gustavo Reyes <gustavosh0154@gmail.com>
2021-11-18 13:16:57 -0600

    configure maps on ios

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -28,6 +28,7 @@ export const PermissionsProvider = ({children}: any) => {
   const [permissions, setPermissions] = useState(permissionInitState);
 
   useEffect(() => {
+    checkLocationPermission();
     AppState.addEventListener('change', state => {
       if (state !== 'active') return;
 
--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export const MapScreen = () => {
   return (
     <View style={{flex: 1}}>
       <MapView
         style={{flex: 1}}
+        provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
